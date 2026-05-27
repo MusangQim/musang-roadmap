e8a1d0e75f09a43deead0fc175ff2909b6959b74
MusangQim <musangqim03@gmail.com>
2026-05-28 00:19:45 +0800

    update: learn about parse and know about command and argument

--- task-tracker-cli/argv_parser.py
+++ task-tracker-cli/argv_parser.py
@@ -1,10 +1,18 @@
 import sys
 
-input = sys.argv
-if len(sys.argv) < 2:
-    print("No command provided")
-    exit
 
-command = sys.argv[1]
-argument = sys.argv[2]
-print(f"{command} {argument}")
+def main() -> None:
+    input_user = sys.argv
+    if len(input_user) < 2:
+        print("No command provided")
+        sys.exit(1)
+    command = sys.argv[1]
+    try:
+        argument = sys.argv[2]
+    except IndexError:
+        argument = None
+    print(f"{command} {argument}")
+
+
+if __name__ == "__main__":
+    main()

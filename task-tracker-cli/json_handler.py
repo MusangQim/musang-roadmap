1726193842d9a7d6a7ecc7bdaa8e65cdc16e095d
MusangQim <musangqim03@gmail.com>
2026-05-28 21:03:06 +0800

    update: learn about open, read, write file in python

--- task-tracker-cli/json_handler.py
+++ task-tracker-cli/json_handler.py
@@ -6,8 +6,8 @@ def load_tasks() -> list:
     if not os.path.exists("tasks.json"):
         with open("tasks.json", 'w') as file:
             file.write("[]")
-        with open("tasks.json", 'r') as files:
-            content = json.load(files)
+    with open("tasks.json", 'r') as files:
+        content = json.load(files)
     return (content)
 
 

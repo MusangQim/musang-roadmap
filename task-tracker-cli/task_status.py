df4e379e033fbe4a2a20c9f4a829ce4153ddac6d
MusangQim <musangqim03@gmail.com>
2026-05-29 23:56:17 +0800

    update: learning about state management on status and filtering done, in-progress, lsit, list done

--- task-tracker-cli/task_status.py
+++ task-tracker-cli/task_status.py
@@ -1,6 +1,7 @@
 from json_handler import load_tasks
-from task_operations import save_tasks
+from task_operations import save_tasks, add_task
 import datetime
+
 
 def mark_status(id, new_status):
     tasks = load_tasks()
@@ -14,19 +15,29 @@ def mark_status(id, new_status):
             break
     if not found:
         print("Error")
+        return
     save_tasks(tasks)
 
 
 def list_tasks(filter=None):
     tasks = load_tasks()
-    if filter == None:
+    if filter is None:
         print(tasks)
     else:
         for task in tasks:
             if task["status"] == filter:
                 print(task)
-            
+
+
 def main() -> None:
-    
+    add_task("Beli roti")
+    add_task("Solat")
+    mark_status(1, "in-progress")
+    mark_status(1, "done")
+    mark_status(99, "done")
+    list_tasks()
+    list_tasks("done")
+
+
 if __name__ == "__main__":
     main()

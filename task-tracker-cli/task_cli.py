dfdb2e774f80e366ce1a288f92d71fe4efa0d657
MusangQim <musangqim03@gmail.com>
2026-05-30 09:32:14 +0800

    update: try assign id_input

--- task-tracker-cli/task_cli.py
+++ task-tracker-cli/task_cli.py
@@ -5,16 +5,17 @@ from task_status import mark_status, list_tasks
 
 def task_cli() -> None:
     command = sys.argv[1]
+    id_input = int(sys.argv[2])
     if command == "add":
         add_task(sys.argv[2])
     elif command == "update":
-        update_task(sys.argv[2], sys.argv[3])
+        update_task(id_input, sys.argv[3])
     elif command == "delete":
-        delete_task(sys.argv[2])
+        delete_task(id_input)
     elif command == "mark-in-progress":
-        mark_status(int(sys.argv[2]), "in-progress")
+        mark_status(id_input, "in-progress")
     elif command == "mark-done":
-        mark_status(int(sys.argv[2]), "done")
+        mark_status(id_input, "done")
     elif command == "list":
         list_tasks(sys.argv[2])
     else:
